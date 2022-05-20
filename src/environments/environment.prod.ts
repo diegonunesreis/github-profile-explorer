c499453f778da42929b2e95646556e19ea72a42a
Diego Nunes Reis <diegonunesftw@gmail.com>
2022-05-19 22:43:13 -0300

    Publish build added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://api.github.com',
+  githubToken: 'Z2hwX0MxYWJpdmFvUlFndWpXNkhBM1V3M1Fjb1NpVWtWTjNoNWhXMA=='
 };
